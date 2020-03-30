9fcbc0aa5de38fbbdf4e014e23bd12692cf59a5d
david <kim.dv@phystech.edu>
2020-03-30 19:10:15 +0300

    Теперь всё должно работать!
    Conflicts:
    	CMakeLists.txt
    	Vector2D.cpp
    	Vector2D.h
    	VectorN.cpp
    	VectorN.h

---
+++ cmake-build-debug/CMakeFiles/VectorN.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VectorN.cpp.dir/Vector2D.cpp.o"
+  "VectorN.cpp"
+  "VectorN.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VectorN.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
